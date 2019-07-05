bcba4b9ef728f6d0bc68ceb2b18af6a790bcf905
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-07-05 16:53:14 +0300

    Add http fetch in axios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
-function App() {
-	return (
-		<div className="App">
-			<Navbar title="Github Finder" icon="fab fa-github" />
-			<div className="container">
-				<Users />
+class App extends Component {
+	state = {
+		users: [],
+		loading: false
+	};
+
+	async componentDidMount() {
+		this.setState({ loading: true });
+		const res = await axios.get('https://api.github.com/users');
+		this.setState({ users: res.data, loading: false });
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Navbar title="Github Finder" icon="fab fa-github" />
+				<div className="container">
+					<Users loading={this.state.loading} users={this.state.users} />
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,32 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-	state = {
-		users: [
-			{
-				id: 'id1',
-				login: 'octocat',
-				avatar_url: 'https://avatars0.githubusercontent.com/u/583231?s=460&v=4',
-				html_url: 'https://github.com/octocat'
-			},
-			{
-				id: 'id2',
-				login: 'octocat2',
-				avatar_url: 'https://avatars0.githubusercontent.com/u/583231?s=460&v=4',
-				html_url: 'https://github.com/octocat'
-			},
-			{
-				id: 'id3',
-				login: 'octocat3',
-				avatar_url: 'https://avatars0.githubusercontent.com/u/583231?s=460&v=4',
-				html_url: 'https://github.com/octocat'
-			}
-		]
-	};
 	render() {
 		return (
 			<div style={userStyle}>
-				{this.state.users.map(user => (
+				{this.props.users.map(user => (
 					<UserItem key={user.id} user={user} />
 				))}
 			</div>
