d79c1244236116656678b180b6fc9088b561914d
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-07-08 10:25:02 +0300

    Add link (React router) in User item

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 class User extends Component {
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
-		console.log(this.props);
 	}
 	render() {
 		return <div>{this.props.user.login}</div>;
